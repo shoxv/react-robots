466aa6dd9969d4f271532ae60c17f56d3de848f9
sebrahim <sebrahim@linkedin.com>
2020-04-25 14:44:49 -0700

    Initial react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import {CardList} from './components/card-list/card-list'
 import './App.css';
+import { FilterBar } from './components/filter-bar/filter-bar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      monsters: [],
+      searchQuery: ''
+    }
+  }
+  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {
+        this.setState({
+          monsters: users,
+          searchQuery: ''
+        })
+      })
+  }
+
+  handleChange(e) {
+    this.setState({searchQuery: e.target.value})
+  }
+
+  render() {
+    const { monsters, searchQuery } = this.state;
+    let filteredMonsters= monsters.filter(monster => monster.name.toLowerCase().includes(searchQuery))
+    return (
+      <div className="App">
+      <h1>Robot Hunter</h1>
+        <FilterBar placeholder="Search Robots" handleChange={this.handleChange} />
+        <ul className="card-list">
+          <CardList cards={filteredMonsters}>Hello govnah</CardList>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
